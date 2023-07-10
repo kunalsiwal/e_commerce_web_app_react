ae895aba2db012a22b8d557da6b020d543e109a3
Kunal Siwal <kunalissiwal@gmail.com>
2023-07-10 21:58:13 +0530

    added search bar and userId dropdown

---
+++ src/components/Dropdowns/NotLoginDropDown.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './NotLoginDropDown.css'
+function NotLoginDropdown(){
+    return (
+        <div className="not-login-dropdown">
+            <button className="button">Login</button>
+            <button className="button signup">Signup</button>
+        </div>
+    )
+}
+
+export default NotLoginDropdown
+
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SearchIcon from '@mui/icons-material/Search';
 import { Link } from "react-router-dom";
+import NotLoginDropdown from "../Dropdowns/NotLoginDropDown";
 
 function Navbar(){
-    console.log(__dirname)
+    
+    const [userIdClicked,setUserIdClicked]=useState(false)
+
+    function handleUserIdClicked(){
+        setUserIdClicked((prev)=>{
+            return !prev;
+        })
+    }
+
     return (
     <div className="navbar">
         <Link to='/' style={{ textDecoration: 'none' ,color:'inherit',display: 'flex'}}>
+        <div className="logo">
             <h1 className='navbar-title'>LoduKart </h1><ShoppingCartIcon fontSize="large" sx={{mt:'5px',pl:'5px',color:'white'}}/>
+        </div>
+            
         </Link>
+        <div className="search-bar">
+            <input type='text' placeholder="search category or product" id="search-input"></input>
+            <SearchIcon fontSize="large" sx={{color:'white'}}/>
+        </div>
+        
         
         <ul className="list">
             <Link to='/About' style={{ textDecoration: 'none' ,color:'inherit'}}>
@@ -31,10 +49,24 @@ function Navbar(){
             </li>
             </Link>
             
-            <li >
+            <li onClick={handleUserIdClicked}>
             <AccountCircleIcon fontSize="large" sx={{marginLeft:'10px',marginTop:'5px'}}/>
             </li>
         </ul>
+        
+        {
+            userIdClicked?
+            <div>
+                <div className="triangle">
+
+                </div>
+                <div className="dropdown">
+                <NotLoginDropdown />
+                </div>
+            </div>
+        :null
+        }
+       
     </div>)
 }
 export default Navbar
